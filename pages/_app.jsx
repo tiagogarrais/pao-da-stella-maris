9d28339906b1220ebaa7bfdd95b952ecedd016c8
tiagogarrais <tiagoarraisholanda@gmail.com>
2025-04-11 17:45:54 -0300

    Adicionando dependência da vercel

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { ...pageProps } }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
